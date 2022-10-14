dbe11c08401bd49e6acad2eccd66487b24189ad3
Sourav Majumder <96306119+Majumder99@users.noreply.github.com>
2022-10-15 00:02:30 +0600

    dynamic array

--- dynamic_array.sol
+++ dynamic_array.sol
@@ -4,6 +4,10 @@ pragma solidity >= 0.4.0 < 0.9.0;
 
 contract dynamicArray{
     uint [] public arr;
+    uint [] public a = [10,20,30,40,50];
+    function returnArray() public view returns(uint[] memory){
+        return a;
+    }
     
     function push(uint item) public{
         arr.push(item);
